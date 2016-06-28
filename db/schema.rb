a69a25519e4f57673a2571dbf79b85b4bc6ec568
Megalapa <megalapa@MBA.local>
2016-06-28 19:07:24 +0300

    add tests

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160628152325) do
   end
 
   create_table "questions", force: :cascade do |t|
+    t.string   "title"
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
