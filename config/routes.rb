5bf25c33c1585752dba1e02f3db265f9a39fe799
Megalapa <megalapa@MBA.local>
2016-07-01 16:15:44 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	
+  devise_for :users
   resources :question, only: [:index, :new, :show, :create] do
     resources :answer
   end
---
+++ spec/acceptance/question/create_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../acceptance_helper'
+
+feature 'create question', %q(
+  In order to get help
+  as a noobie user
+  I whant to be able create question) do
+  scenario ''
+  	
+end
