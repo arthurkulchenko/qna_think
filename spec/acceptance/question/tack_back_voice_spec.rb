4a1cec4b6158ffc91b44632d664b12b124262310
Megalapa <megalapa@MBA.local>
2016-08-29 13:55:17 +0300

    done

--- spec/acceptance/question/tack_back_voice_spec.rb
+++ spec/acceptance/question/tack_back_voice_spec.rb
@@ -6,9 +6,9 @@ feature 'unvote', %q(
   I wish to be able tack back my voice
 ) do
   given(:user){ create(:user) }
-  given(:question){ create(:question, user: user) }
-  given!(:answer){ create(:answer, question: question) }
-  given!(:vote){ create(:voted_plus, ballot_id: answer.id, ballot_type: answer.class, user: user) }
+  given(:question){ create(:question) }
+  # given!(:answer){ create(:answer, question: question) }
+  given!(:vote){ create(:voted_plus, ballot_id: question.id, ballot_type: question.class, user: user) }
 
   background do
     sign_in(user)
@@ -16,7 +16,7 @@ feature 'unvote', %q(
   end
 
   scenario 'user try to unvote' do
-    # save_and_open_page
+    save_and_open_page
     expect(page).to have_link "Take back your voice"
     click_on "Take back your voice"
     visit question_path(question)
--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do
   factory :vote do
-    
+    ballot
+    user
+    mark 0
+  end
+
+  factory :voted_plus, class: Vote do
+    user 
+    mark 1
+  end
+
+  factory :voted_minus, class: Vote do
+    user 
+    mark -1
   end
 end
