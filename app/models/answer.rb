b62e33a95c4edaa8c65a155c3c21ff25535cf1ca
Megalapa <megalapa@MBA.local>
2016-09-24 19:52:19 +0300

    fix first answer best

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -14,16 +14,10 @@ class Answer < ApplicationRecord
   scope :best_first, -> { order(best_answer: :desc) }
   scope :not_best_answers, -> { where(best_answer: false) }
 
-  def answers_stack
-    question.answers
-  end
-
-  # @answers_with_filter = @answer.answers_stack.best_first.includes(:attachments)
-
   private
   
   def check_of_best
-    return unless @best_answer = question.answers.where(best_answer: true).first
+    return unless @best_answer = question.answers.best_first.first
     return @best_answer.toggle(:best_answer).save unless ( id == @best_answer.id ) || ( !best_answer )
   end
 
