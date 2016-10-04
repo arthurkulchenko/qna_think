edac2d84ddf0fb025c18d1b27a467c510b356057
Megalapa <megalapa@MBA.local>
2016-10-04 16:08:18 +0300

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,77 +1,13 @@
-# class User < ApplicationRecord
-#   devise :database_authenticatable, :registerable, :recoverable, :rememberable, # :confirmable, :lockable, :timeoutable
-#          :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook, :twitter]
-  
-#   [:questions, :answers, :votes, :comments, :attachments, :authorizations].each do |model|
-#     has_many model, dependent: :destroy
-#   end
-
-#   def is_author_of?(obj)
-#     obj.user_id == id
-#   end
-
-#   def merge_this(user)
-#     user.authorizations.each do |auth|
-#       new_attr = auth.attributes
-#       new_attr.delete("id")
-#       authorizations.create!(new_attr)
-#     end
-#     user.destroy
-#     save
-#     self
-#   end
-
-#   class << self
-#     def find_by_auth(req)
-#       authorization = Authorization.where(provider: req.provider, uid: req.uid.to_s).first
-#       authorization.user if authorization
-#     end
-
-#     def find_by_email(req)
-#       token = SecureRandom.base64(18)
-#       user = User.find_by(email: req.info[:email]) if req.info.has_key?(:email)
-#       if user && user.authorizations.empty?
-#         user.authorizations.create!(provider: req.provider, uid: req.uid.to_s, confirmation_token: token)
-#       end
-#       user
-#     end
-  
-#     def creating_new(req)
-#       password = Devise.friendly_token[0..20]
-#       token = SecureRandom.base64(18)
-#       if req.info.has_key?(:email)
-#         r_email = true
-#         g_email = req.info[:email]
-#       else
-#         r_email = false
-#         g_email = "#{req.provider}-#{req.uid}@email.com"
-#       end
-#       user = User.create!(email: g_email, password: password, password_confirmation: password, email_real: r_email )
-#       user.authorizations.create!(provider: req.provider, uid: req.uid.to_s, confirmation_token: token)
-#       user
-#     end
-  
-#     def find_for_oauth(req)
-#       if User.find_by_auth(req)
-#         User.find_by_auth(req)
-#       else
-#         if User.find_by_email(req)
-#           User.find_by_email(req)
-#         else
-#           User.creating_new(req)
-#         end
-#       end
-#     end
-#   end
-
-  
-# end
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, # :confirmable, :lockable, :timeoutable
          :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook, :twitter]
   
   [:questions, :answers, :votes, :comments, :attachments, :authorizations].each do |model|
     has_many model, dependent: :destroy
+  end
+
+  def is_author_of?(obj)
+    obj.user_id == id
   end
   
   def self.find_for_oauth(req)
@@ -90,7 +26,6 @@ class User < ApplicationRecord
     # Если нет не на авторизации не почты, то создаем и то и другое
     else
       token_and_password_generating
-      @token = SecureRandom.base64(18)
       email_genarating(req)
       user = User.create!(email: @g_email, password: @password, password_confirmation: @password, email_real: @r_email )
       user.authorizations.create!(provider: req.provider, uid: req.uid.to_s, confirmation_token: @token)
@@ -98,25 +33,25 @@ class User < ApplicationRecord
     end
   end
 
-  def self.token_and_password_generating
-    
-    @password = Devise.friendly_token[0..20]      
-  end
-
   private
 
-  def self.email_genarating(req)
-    if req.info.has_key?(:email)
-      @r_email = true
-      @g_email = req.info[:email]
-    else
-      @r_email = false
-      @g_email = "#{req.provider}-#{req.uid}@email.com"
-    end
-  end
+  class << self
 
-  def is_author_of?(obj)
-    obj.user_id == id
+    def email_genarating(req)
+      if req.info.has_key?(:email)
+        @r_email = true
+        @g_email = req.info[:email]
+      else
+        @r_email = false
+        @g_email = "#{req.provider}-#{req.uid}@email.com"
+      end
+    end
+  
+    def token_and_password_generating
+      @token = SecureRandom.base64(18)
+      @password = Devise.friendly_token[0..20]      
+    end
+
   end
 
   def merge_this(user)
