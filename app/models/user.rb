b4187b4077a5a795b7a2b5cae1123ae006153197
Megalapa <megalapa@MBA.local>
2016-10-10 13:48:52 +0300

    transaction add

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   def self.find_for_oauth(req)
     authorization = Authorization.where(provider: req.provider, uid: req.uid.to_s).first
     return authorization.user if authorization
-    # User.transaction do
+    User.transaction do
       if req.info.has_key?(:email) && user = User.find_by(email: req.info[:email])
         user
       else
@@ -23,7 +23,7 @@ class User < ApplicationRecord
         user = User.create!(email: @g_email, password: @password, password_confirmation: @password, email_real: @r_email )
       end
       user.authorizations.create!(provider: req.provider, uid: req.uid.to_s) if user.authorizations.empty?
-    # end
+    end
     user
   end
 
