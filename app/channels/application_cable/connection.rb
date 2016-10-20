c83d5b9045dfb2d0e3f4306533658a942203eb1e
Megalapa <megalapa@MBA.local>
2016-10-20 23:42:00 +0300

    ...

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -5,20 +5,19 @@ module ApplicationCable
  
     def connect
       self.current_user = find_verified_user
-      logger.add_tags env['warden'].user
-      logger.add_tags current_user
-      logger.add_tags 'ActionCable', current_user.email
+      # logger.add_tags env['warden'].user
+      # logger.add_tags current_user
+      # logger.add_tags 'ActionCable', current_user.email
     end
  
     protected
 
-    def find_verified_user
-      verified_user = env['warden'].user ? verified_user : reject_unauthorized_connection
-      # if current_user = User.find_by(id: cookies.signed[:user_id])
-      #     current_user
-      #   else
-      #     reject_unauthorized_connection
-      #   end
+    def find_verified_user      # verified_user = env['warden'].user ? verified_user : reject_unauthorized_connection
+      if current_user = User.find_by(id: cookies.signed[:user_id])
+        current_user
+      else
+        reject_unauthorized_connection
+      end
     end
 
   end
