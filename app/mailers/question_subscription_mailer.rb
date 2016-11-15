fdd6b9772e1ee5562818bb5a5a9643d7a86d9d15
Megalapa <megalapa@MBA.local>
2016-11-15 17:30:37 +0300

    mail changings

--- app/mailers/question_subscription_mailer.rb
+++ app/mailers/question_subscription_mailer.rb
@@ -1,13 +1,19 @@
 class QuestionSubscriptionMailer < ApplicationMailer
   def send_digest(user, questions)
+    @user = user
+    @questions = questions
     mail(to: user.email, subject: 'QA_thinknetika Digest')
   end
 
   def new_answer_letter(user, answer)
+    @user = user
+    @answer = answer
     mail(to: user.email, subject: 'You have new answer')
   end
 
   def question_newslettering(user, question)
+    @user = user
+    @question = question
     mail(to: user.email, subject: 'You have new answer')	
   end
 end
