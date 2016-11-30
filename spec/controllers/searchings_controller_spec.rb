dd0fad87a32495a713dd84a25cd1114dafbb85d1
Megalapa <megalapa@MBA.local>
2016-11-30 15:23:44 +0300

    before changing acceptance

--- spec/controllers/searchings_controller_spec.rb
+++ spec/controllers/searchings_controller_spec.rb
@@ -4,29 +4,21 @@ RSpec.describe SearchingsController, type: :controller do
   describe "POST#create" do
     %w(global user question answer comment).each do |e|
       context "in #{e} search" do
-        # it_behaves_like "SearchController"
         let(:attr_is){ e == "user" ? :email : :content }
         let(e.to_sym){ create(e.to_sym, attr_is => "pancakes") }
+        
         let(:search){ThinkingSphinx.search 'pancakes', :indices => ["#{e}_core"]}
-        it "render #{e}" do
-          expect{search}
-        end
+        let(:request) { post :create, "area"=> e.capitalize, "query"=>"pancakes", format: :json }
+        
+        # it "searching" do
+        	# , :indices => ["#{e}_core"]
+          # expect(ThinkingSphinx).to receive(:search).with("pancakes").and_call_original
+        #   request
+        # end
+
+        before { request }
+        it_behaves_like "Success status"
       end
     end
-    # context "in global search" do
-    #   it_behaves_like "SearchController"
-    # end
-    # context "in user search" do
-    #   it_behaves_like "SearchController"
-    # end
-    # context "in question search" do
-    #   it_behaves_like "SearchController"
-    # end
-    # context "in answer search" do
-    #   it_behaves_like "SearchController"
-    # end
-    # context "in comment search" do
-    #   it_behaves_like "SearchController"
-    # end
   end
 end
