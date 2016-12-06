2c59b20c6a02ce3ce71124d8bdd0e32ffeaec79c
Megalapa <megalapa@MBA.local>
2016-12-06 13:44:22 +0300

    try to fix search problem

--- app/controllers/searchings_controller.rb
+++ app/controllers/searchings_controller.rb
@@ -1,6 +1,6 @@
 class SearchingsController < ApplicationController
   skip_authorization_check
-
+  respond_to :json
   def create
     respond_to do |format|
       format.json { render json: Search.for(params[:query], params[:area]) }
